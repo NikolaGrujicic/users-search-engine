b0fbbcc007fd75e86aa0779e5ab54a3ad4ac4d74
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-13 12:41:19 +0100

    components structure setup

---
+++ src/components/customers/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function customersList() {
+  return (
+    <div />
+  );
+}
+
+export default customersList;
---
+++ src/components/navigations/navBar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TopNavBar() {
+  return (
+    <div />
+  );
+}
+
+export default TopNavBar;
