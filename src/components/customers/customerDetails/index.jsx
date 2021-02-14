0dae7b98a3f8ac825ebf8a5d2356d1638621c2d3
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-14 13:39:34 +0100

    Delete old customers directory

--- src/components/customers/customerDetails/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CustomerModal() {
-  return (
-    <div />
-  );
-}
-
-export default CustomerModal;
--- src/components/customers/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function customersList() {
-  return (
-    <div />
-  );
-}
-
-export default customersList;
