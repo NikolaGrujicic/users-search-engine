ff35f6c2877b32f815d882f159817950551e3e74
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-17 22:00:19 +0100

    added router to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Users from './components/Users';
 import './styles/_main.scss';
 
 const App = () => (
-  <>
-    <Header />
-    <Users />
-  </>
+  <Router>
+    <>
+      <Header />
+      <Users />
+      <Switch>
+        <Route path="/users" />
+      </Switch>
+    </>
+  </Router>
 );
 
 export default App;
