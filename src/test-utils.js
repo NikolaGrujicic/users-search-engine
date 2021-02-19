1853ae2582e1ae3efb587de2c25204c8b739d0c1
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-19 07:56:13 +0100

    replaced enzyme by react-testing-library

---
+++ src/test-utils.js
@@ -0,0 +1,11 @@
+import { rtl } from '@testing-library/react';
+
+const customRender = (ui, options) => rtl.render(ui, {
+  myDefaultOption: 'something',
+  ...options,
+});
+
+module.exports = {
+  ...rtl,
+  render: customRender,
+};
