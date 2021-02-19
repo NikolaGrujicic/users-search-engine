29298dee843e85b4e97e96530f8cc57bdb1c9c83
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-19 07:34:44 +0100

    modal test added

--- src/components/Users/UsersDetailsModal/__tests__/UsersDetailsModal.test.jsx
+++ src/components/Users/UsersDetailsModal/__tests__/UsersDetailsModal.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import UsersDetailsModal from '../UsersDetailsModal';
 
 test('renders Users Details Modal', () => {
@@ -7,4 +7,39 @@ test('renders Users Details Modal', () => {
     <UsersDetailsModal />,
   );
   expect(wrapper).toMatchSnapshot();
+});
+
+test('renders Users Details Modal with props', () => {
+  const wrapper = shallow(
+    <UsersDetailsModal
+      state="California"
+      city="Los Angeles"
+      postcode="123456"
+      street="Queens Boulevard"
+      phone="555-333"
+      cell="333-555"
+    />,
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('check Users Details Modal props are correct', () => {
+  const wrapper = mount(
+    <UsersDetailsModal
+      state="California"
+      city="Los Angeles"
+      postcode="123456"
+      street="Queens Boulevard"
+      phone="555-333"
+      cell="333-555"
+    />,
+  );
+
+  expect((wrapper).prop('state')).toEqual('California');
+  expect((wrapper).prop('city')).toEqual('Los Angeles');
+  expect((wrapper).prop('postcode')).toEqual('123456');
+  expect((wrapper).prop('street')).toEqual('Queens Boulevard');
+  expect((wrapper).prop('phone')).toEqual('555-333');
+  expect((wrapper).prop('cell')).toEqual('333-555');
 });
