33ee99c40717257db73d87e612801bf2906c6c9b
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-19 14:31:42 +0100

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import PageNotFound from './components/PageNotFound/PageNotFound';
+import PageNotFound from './components/PageNotFound';
 import Settings from './components/Settings';
 import Users from './components/Users';
 import './styles/_main.scss';
