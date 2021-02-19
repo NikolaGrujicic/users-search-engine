b3fe6fea68589974b38dc8baf1b0878b425a868a
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-19 14:29:46 +0100

    changed app name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import SearchBar from '../SearchBar/SearchBar';
 
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a className="navbar-brand" href="/">Users Search Engine</a>
+    <a className="navbar-brand" href="/">Address book</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
@@ -12,7 +12,7 @@ const Header = () => (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <Link className="nav-link" to="/users">
+          <Link className="nav-link" to="/">
             Users
             <span className="sr-only">(current)</span>
           </Link>
